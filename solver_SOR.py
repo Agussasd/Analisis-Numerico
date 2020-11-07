2d1120fe52a380bb5972c8f48dc07b5b54baee8d
Aguss- <56332666+Agussasd@users.noreply.github.com>
2020-11-07 19:01:15 -0300

    Update solver_SOR.py

--- solver_SOR.py
+++ solver_SOR.py
@@ -5,11 +5,11 @@ K = [[4.0, -1.0, -1.0, 0.0], [-1.0, 4.0, 0.0, -1.0], [-1.0, 0.0, 4.0, -1.0], [0.
 v = [1.0, 2.0, 0.0, 1.0]
 x = [0.0, 0.0, 0.0, 0.0]
 #Resultado = [0.5, 0.75, 0.25, 0.5]
-tol = 0.1
-w = 1.2
+tol = 0.01
+w = 1.4
 n = 4 #dimension de matriz
 
-def SOR(A, b):
+def solver_SOR(A, b):
 	error = 2  * tol
 	diferencia = np.zeros(n, dtype = float)
 	iteraciones = 0
@@ -18,11 +18,13 @@ def SOR(A, b):
 			suma = 0.0
 			suma2 = 0.0
 			for j in range(0, i):
-				suma = suma + A[i][j] * x[j]
+				if j != i:
+					suma = suma + A[i][j] * x[j]
 			for j in range(i, n):
-				suma2 = suma2 + A[i][j] * x[j]
+				if j != i:
+					suma2 = suma2 + A[i][j] * x[j]
 			R = b[i] - suma - suma2
-			nuevo = x[i] * (1 - w)+ (w / A[i][i]) * R
+			nuevo = x[i] * (1 - w) + (w / A[i][i]) * R
 			diferencia[i] = np.abs(nuevo - x[i])
 			x[i] = nuevo
 
@@ -32,4 +34,4 @@ def SOR(A, b):
 	print(iteraciones)
 	print(x)
 
-SOR(K, v)
+solver_SOR(K, v)
