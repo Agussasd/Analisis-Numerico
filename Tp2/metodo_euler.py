2d4a51ce250ddb30abaf4dc2836ffa7c308cf6dd
Aguss- <56332666+Agussasd@users.noreply.github.com>
2021-02-07 18:59:12 -0300

    Update metodo_euler.py

--- Tp2/metodo_euler.py
+++ Tp2/metodo_euler.py
@@ -4,12 +4,14 @@ from matplotlib import pyplot as plt
 
 def FX(x, y):
 	"""dVx/dt"""
-	ecuacion_resuelta = (6.674e-11) * ((5972e+21)/(-4.670e+6-x)**2 + ((-y)**2)) * (math.cos(math.atan2(-y, (-4.670e+6 - (x)))))
+	alf1 = np.arctan2(-y, (-4.670e+6-(x)))
+	ecuacion_resuelta = (6.674e-11) * ((5972e+21)/(-4.670e+6-x)**2) + ((-y)**2) * np.cos(alf1)
 	return ecuacion_resuelta
 
 def FY(x, y):
 	"""dVy/dt"""
-	ecuacion_resuelta = (6.674e-11) * ((5972e+21)/(((-4.670e+6-x)**2) + ((-y)**2))) * (np.sin(np.arctan2(-y, -4.670e+6 - x)))
+	alf1 = np.arctan2(-y, (-4.670e+6 -(x)))
+	ecuacion_resuelta = (6.674e-11) * ((5972e+21)/(-4.670e+6-x)**2) + ((-y)**2) * np.sin(alf1)
 	return ecuacion_resuelta
 
 def metodo_de_euler(h, tf):
@@ -33,10 +35,9 @@ def metodo_de_euler(h, tf):
 		x[i + 1] = x[i] + h * vx[i + 1]
 		y[i + 1] = y[i] + h * vy[i + 1]
 
+
 	plt.plot(x, y)
 	plt.xlabel('x')
 	plt.ylabel('y')
 	plt.title('Trayectoria')
 	plt.show()
-
-metodo_de_euler(1, 5798.779242)
